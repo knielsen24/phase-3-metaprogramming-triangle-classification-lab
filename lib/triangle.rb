5adffefaec8ab8f31f782111f0704ed6df24415c
Kevin Nielsen <knielsen24@gmail.com>
2022-06-09 13:41:24 -0700

    commit tree 964b4b3702894f6d23178e06e23f2484c77f9eb7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
+require 'pry'
+
+# the class needs 3 arguments
+# two sides must be greater than the 3rd
+
 class Triangle
-  # write code here
+	attr_reader :a, :b, :c
+	attr_writer :kind
+
+	def initialize(a, b, c)
+		@a = a
+		@b = b
+		@c = c
+		@kind = self.kind
+	end
+
+	def kind
+		if @a <= 0 || @b <= 0 || @c <= 0
+			raise TriangleError
+		elsif @a + @b <= @c || @b + @c <= @a || @c + @a <= @b
+			raise TriangleError
+		elsif @a == @b && @b == @c
+			:equilateral
+		elsif @a != @b && @b != @c && @c != @a
+			:scalene
+		elsif @a == @b || @b == @c || @c == @a
+			:isosceles
+		end
+	end
+
+	class TriangleError < StandardError
+	end
+
 end
+
+# equilateral = Triangle.new(2, 2, 2)
+# isoceles = Triangle.new(2, 3, 3)
+# scalene = Triangle.new(1, 2, 3)
+
+
+def reload
+	load "lib/triangle.rb"
+end
+
+binding.pry
+
